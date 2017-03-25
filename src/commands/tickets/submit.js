ecf515634ebed75802fc7b01290b34728434689d
Brandan 'Nomsy' Palmer <bpalmer647@gmail.com>
2017-03-25 01:46:20 -0400

    added tickets:submit. works okay

--- src/commands/tickets/submit.js
+++ src/commands/tickets/submit.js
@@ -1,5 +1,17 @@
-const { Command } = require('discord.js-commando');
+const {
+    Command
+} = require('discord.js-commando');
+const {
+    RichEmbed
+} = require('discord.js');
+const {
+    oneLine
+} = require('common-tags');
+
+const moment = require('moment');
 const winston = require('winston');
+const ticketModel = require('../../database/models/tickets');
+const config = require('../../../settings');
 
 module.exports = class SubmitCommand extends Command {
     constructor(client) {
@@ -14,24 +26,81 @@ module.exports = class SubmitCommand extends Command {
                 usages: 1,
                 duration: 60
             },
-			args: [
-				{
-					key: 'reportType',
-					prompt: 'What type of report are you submitting? (feature, bug, general)',
-					type: 'string'
-				},
+            args: [{
+                    key: 'reportType',
+                    prompt: 'What type of report are you submitting? (feature, bug, general)',
+                    type: 'string'
+                },
                 {
-       				key: 'reportMessage',
-					prompt: 'What are you reporting?',
-					type: 'string'             
+                    key: 'reportMessage',
+                    prompt: 'What are you reporting?',
+                    type: 'string'
                 }
-			]
+            ]
         });
     }
 
     async run(msg, args) {
-        await statusMessage = msg.say('Submitting ticket...');
+        let ticket = {
+            userId: msg.author.id,
+            username: `${msg.author.username}#${msg.author.discriminator}`,
+            ticketType: args.reportType,
+            message: args.reportMessage
+        };
 
-        // TODO: Actually submit a ticket.
+        this.submitTicket(ticket);
+        this.sendTicketMessage(ticket, msg);
+    }
+
+    ticketMessage(data) {
+        return data.message;
+    }
+
+    ticketUserId(data) {
+        return data.userId;
+    }
+
+    ticketUsername(data) {
+        return data.username;
+    }
+
+    ticketType(data) {
+        return data.type;
+    }
+
+    ticketData(data) {
+        return {
+            userId: data.userId,
+            username: data.username,
+            type: data.ticketType,
+            message: data.message
+        }
+    }
+
+    submitTicket(data) {
+        let ticket = this.ticketData(data);
+        return ticketModel.create({
+            userId: this.ticketUserId(ticket),
+            userName: this.ticketUsername(ticket),
+            ticketType: this.ticketType(data),
+            content: this.ticketMessage(ticket)
+        }).catch((e) => {
+            winston.error(`Error occured! ${e}`);
+        });
+    }
+
+    sendTicketMessage(data, message) {
+        if (config.tickets.reportToChannel == true) {
+            let ticket = this.ticketData(data);
+            const embed = new RichEmbed()
+                .setAuthor(`${this.ticketUsername(ticket)}`, message.author.avatarURL)
+                .setColor(0x00AE86)
+                .setDescription(`**Content**: ${this.ticketMessage(ticket)}`)
+                .setFooter(`${moment().tz('America/New_York').format('LTS')} | Type: ${this.ticketType(ticket)}`);
+            this.client.channels.get(config.tickets.channelId).sendEmbed(embed);
+            message.reply('Ticket submitted!');
+        } else {
+            message.reply('Ticket submitted, it will be viewed on our panel/system.');
+        }
     }
 }
--- src/database/models/tickets.js
+++ src/database/models/tickets.js
@@ -1,17 +1,19 @@
 const { Sequelize } = require('sequelize');
 const Database = require('../database');
+const moment = require('moment');
 const database = new Database();
 
 const tickets = database.db.define('tickets', {
-    id: { 
-        type: Sequelize.INTEGER, 
-        primaryKey: true, 
-        autoIncrement: true 
-    },
     userId: Sequelize.STRING,
     userName: Sequelize.STRING,
     ticketType: Sequelize.STRING,
-    content: Sequelize.STRING(1800)
+    content: Sequelize.STRING(1800),
+    date: {
+        type: Sequelize.DATEONLY,
+        get: function () {
+            return moment.utc(this.getDataValue('date')).format('YYYY-MM-DD');
+        }
+    }
 });
 
 tickets.sync();
